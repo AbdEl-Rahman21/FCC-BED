e1320a608ec75c58cb15d3ce55981cd394cf251e
AbdEl-Rahman21 <abdel292004@gmail.com>
2024-10-18 00:15:26 +0300

    Complete the URL Shortener Microservice

--- URL Shortener Microservice/index.js
+++ URL Shortener Microservice/index.js
@@ -1,7 +1,12 @@
 require('dotenv').config();
+const bodyParser = require('body-parser');
 const express = require('express');
+const URL = require('url').URL;
 const cors = require('cors');
+const dns = require('dns');
 const app = express();
+
+const shortUrls = [];
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -17,6 +22,28 @@ app.get('/', function(req, res) {
 // Your first API endpoint
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
+});
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.post('/api/shorturl', (req, res) => {
+  const url = new URL(req.body.url);
+
+  dns.lookup(url.hostname, (err) => {
+    if (err) {
+      res.json({error: 'invalid url'});
+    } else if (shortUrls.includes(req.body.url)) {
+      res.json({original_url: req.body.url, short_url: shortUrls.indexOf(req.body.url) + 1});
+    } else {
+      shortUrls.push(req.body.url);
+
+      res.json({original_url: req.body.url, short_url: shortUrls.length});
+    }
+  });
+});
+
+app.get('/api/shorturl/:shortUrl?', (req, res) => {
+  res.redirect(shortUrls[req.params.shortUrl - 1]);
 });
 
 app.listen(port, function() {
