b8de985e88d04e9df3abe667e5251627e4553cd9
AbdEl-Rahman21 <abdel292004@gmail.com>
2024-10-19 00:33:28 +0300

    Complete File Metadata Microservice

--- File Metadata Microservice/index.js
+++ File Metadata Microservice/index.js
@@ -1,8 +1,12 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+require('dotenv').config();
 
-var app = express();
+const cors = require('cors');
+const express = require('express');
+const multer = require('multer');
+
+const app = express();
+const port = process.env.PORT || 3000;
+const upload = multer({dest: './public'});
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,10 +15,14 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  });
+});
 
-
-
-const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log('Your app is listening on port ' + port);
 });
